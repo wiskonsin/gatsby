25cfb3fdc838c37681152cdf2f9726ab0ddd6305
Luis Alberto Riancho Martín <luisriancho@gmail.com>
2018-10-22 01:24:47 +0200

    actualización

---
+++ .cache/async-requires.js
@@ -0,0 +1,13 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/Luigi/Library/Mobile Documents/com~apple~CloudDocs/Business/Gatsby/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-about-js": () => import("/Users/Luigi/Library/Mobile Documents/com~apple~CloudDocs/Business/Gatsby/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-contact-js": () => import("/Users/Luigi/Library/Mobile Documents/com~apple~CloudDocs/Business/Gatsby/src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
+  "component---src-pages-index-js": () => import("/Users/Luigi/Library/Mobile Documents/com~apple~CloudDocs/Business/Gatsby/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-tutorial-js": () => import("/Users/Luigi/Library/Mobile Documents/com~apple~CloudDocs/Business/Gatsby/src/pages/tutorial.js" /* webpackChunkName: "component---src-pages-tutorial-js" */)
+}
+
+exports.data = () => import("/Users/Luigi/Library/Mobile Documents/com~apple~CloudDocs/Business/Gatsby/.cache/data.json")
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,12 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/Luigi/Library/Mobile Documents/com~apple~CloudDocs/Business/Gatsby/.cache/dev-404-page.js")),
+  "component---src-pages-about-js": preferDefault(require("/Users/Luigi/Library/Mobile Documents/com~apple~CloudDocs/Business/Gatsby/src/pages/about.js")),
+  "component---src-pages-contact-js": preferDefault(require("/Users/Luigi/Library/Mobile Documents/com~apple~CloudDocs/Business/Gatsby/src/pages/contact.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/Luigi/Library/Mobile Documents/com~apple~CloudDocs/Business/Gatsby/src/pages/index.js")),
+  "component---src-pages-tutorial-js": preferDefault(require("/Users/Luigi/Library/Mobile Documents/com~apple~CloudDocs/Business/Gatsby/src/pages/tutorial.js"))
+}
+
