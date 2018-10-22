ad48a4e40ce6804af6dc64df65646bcba08e512e
wiskonsin <luisriancho@gmail.com>
2018-10-22 19:59:32 +0200

    contacto bien

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,8 +4,8 @@ import Layout from "../components/layout.js"
 export default () => (
     <Layout>
   <div>
-    <h1>Para cualquier duda o sugerencia no dudes en enviarme un email a:</h1>
-    <p>
+    <h1>Contacto</h1>
+    <p>Para cualquier duda o sugerencia no dudes en enviarme un email a 
       <a href="mailto:luisriancho@gmail.com">luisriancho@gmail.com</a>
     </p>
   </div>
