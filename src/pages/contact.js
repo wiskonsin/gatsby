4731dbef704144532f22a211bb316a23d9f668eb
Luis Riancho <l.riancho@diatomea.tech>
2018-10-23 08:27:54 +0200

    correción correo electrónico

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,8 +5,7 @@ export default () => (
     <Layout>
   <div>
     <h1>Contacto</h1>
-    <p>Para cualquier duda o sugerencia no dudes en enviarme un email a 
-      <a href="mailto:luisriancho@gmail.com">luisriancho@gmail.com</a>
+    <p>Para cualquier duda o sugerencia no dudes en enviarme un email a <span><a href="mailto:luisriancho@gmail.com">luisriancho@gmail.com</a></span>
     </p>
   </div>
   </Layout>
