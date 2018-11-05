ff30220d28d42df46aa6071aa60e175dd139d960
Luis Alberto Riancho Martín <luisriancho@gmail.com>
2018-11-05 23:37:34 +0100

    anuncios

--- src/components/ads.js
+++ src/components/ads.js
@@ -1,8 +1,9 @@
 import React from "react"
+import CssStyles from "../css/index-css-modules.module.css"
 
 export default ({client, format, slot,responsive = 'auto'}) => (
     <div>
-    <ins className="adsbygoogle"
+    <ins  id={CssStyles.adsbygoogle} className="adsbygoogle"
         
     data_ad_client={client}
     data_ad_slot = {slot}
